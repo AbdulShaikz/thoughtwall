63d8fd82e8902b57d6f2e5ff5cdf7755ab0db6b5
AbdulShaikz <starabdul100@gmail.com>
2026-05-06 21:10:14 +0530

    initial commit: backend setup

---
+++ backend/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+
+const PORT = 5000
+app.get('/', (req,res) => {
+    return res.json({
+        status: 'ok',
+    })
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on PORT: ${PORT}`);
+})
